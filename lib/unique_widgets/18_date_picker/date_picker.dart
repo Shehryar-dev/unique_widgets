aefb9c2436f86d933d269ca109d94994126fd0e3
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-04-20 13:59:25 +0500

    Code Updated

---
+++ lib/unique_widgets/18_date_picker/date_picker.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class DatePickerExample extends StatefulWidget {
+  const DatePickerExample({super.key});
+
+  @override
+  State<DatePickerExample> createState() => _DatePickerExampleState();
+}
+
+class _DatePickerExampleState extends State<DatePickerExample> {
+  DateTime? selectedDate;
+
+  Future<void> _pickDate() async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate ?? DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Date Picker Example')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              selectedDate != null
+                  ? 'Selected Date: ${selectedDate!.toLocal()}'.split(' ')[0]
+                  : 'No date selected',
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton.icon(
+              onPressed: _pickDate,
+              icon: const Icon(Icons.date_range),
+              label: const Text('Pick a Date'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
