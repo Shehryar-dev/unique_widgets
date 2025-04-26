e69a35e001323e1e8f4a0cd48f355640cd24fc64
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-04-26 14:54:50 +0500

    Code Update

---
+++ lib/unique_widgets/24_adaptive_widgets/adaptive_widgets.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class AdaptiveWidgetExample extends StatelessWidget {
+  const AdaptiveWidgetExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Adaptive Widgets Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            // Adaptive Switch
+            Switch.adaptive(
+              value: true,
+              onChanged: (bool value) {},
+            ),
+            const SizedBox(height: 20),
+
+            // Adaptive CircularProgressIndicator
+            const CircularProgressIndicator.adaptive(),
+
+            const SizedBox(height: 20),
+
+            // Adaptive Icon Button
+            IconButton(
+              icon: const Icon(Icons.adaptive.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
