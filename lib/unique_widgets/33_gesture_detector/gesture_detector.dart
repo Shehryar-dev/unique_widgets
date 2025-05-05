0e8f1f5de274736052292eace78dafb6aafca618
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-05-05 13:25:14 +0500

    Code Update

---
+++ lib/unique_widgets/33_gesture_detector/gesture_detector.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class GestureDetectorExample extends StatefulWidget {
+  const GestureDetectorExample({super.key});
+
+  @override
+  State<GestureDetectorExample> createState() => _GestureDetectorExampleState();
+}
+
+class _GestureDetectorExampleState extends State<GestureDetectorExample> {
+  String message = '👆 Tap, Double Tap or Long Press the Card';
+
+  void _handleGesture(String type) {
+    setState(() {
+      message = type;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF3F4F6),
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: const Text('GestureDetector Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () => _handleGesture('✅ Single Tap Detected'),
+              onDoubleTap: () => _handleGesture('🔁 Double Tap Detected'),
+              onLongPress: () => _handleGesture('⏳ Long Press Detected'),
+              child: Card(
+                elevation: 8,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Image.network(
+                    'https://picsum.photos/250?image=9',
+                    width: 250,
+                    height: 180,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              message,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.teal,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
