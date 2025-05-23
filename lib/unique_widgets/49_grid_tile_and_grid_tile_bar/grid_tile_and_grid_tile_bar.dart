7a976421319810e2c499917de4dcc9784a236c27
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-05-23 12:29:59 +0500

    Code Update

---
+++ lib/unique_widgets/49_grid_tile_and_grid_tile_bar/grid_tile_and_grid_tile_bar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class GridTileExample extends StatelessWidget {
+  final List<Map<String, dynamic>> products = [
+    {
+      "title": "Nike Shoes",
+      "price": "\$120",
+      "image": "https://i.imgur.com/2yaf2wb.png"
+    },
+    {
+      "title": "Adidas Sneakers",
+      "price": "\$98",
+      "image": "https://i.imgur.com/L68FtMA.png"
+    },
+    {
+      "title": "Puma Runner",
+      "price": "\$89",
+      "image": "https://i.imgur.com/ODkQn6B.png"
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Product Grid")),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: GridView.builder(
+          itemCount: products.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            childAspectRatio: 3 / 4,
+          ),
+          itemBuilder: (context, index) {
+            final product = products[index];
+            return GridTile(
+              footer: GridTileBar(
+                backgroundColor: Colors.black87,
+                title: Text(product['title']),
+                subtitle: Text(product['price']),
+                trailing: IconButton(
+                  icon: const Icon(Icons.favorite_border, color: Colors.white),
+                  onPressed: () {},
+                ),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  product['image'],
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
